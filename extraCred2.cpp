6dc59828efb6684500fbac5e625a9b4b4259ad57
Mays <kmays@storm.cis.fordham.edu>
2018-01-06 12:56:14 -0500

    Updating

--- extraCred2.cpp
+++ extraCred2.cpp
@@ -1,27 +1,49 @@
 // Katie Mays
-// November 12, 2017
-// Midterm Extra Credit 2: reverse words and phrases
+// Nov. 16, 2017
+// Midterm Extra Credit 2: reverse string
 
 #include <iostream>
+#include <sstream>
+#include <string>
 using namespace std;
 
 int main()
 {
-	// variables
+	// vars
+	int i = 0;
 	string phrase;
-	
-	// input
-	cout << "Enter your sentence, word, or phrase: \n";
-	getline(cin, phrase);
-	
-	// processing
-	for (int i = 0; i < phrase.length(); i++)
-	{
-		for (int j = phrase.length() - 1; j >= 0; j--)
-		{
-			cout << phrase.at(j);
-		}
-	}
-	return 0;
-}
+	char repeat;
+		
+	// greeting
+	cout << "Welcome to Reverse-the-Words!" << endl;
 
+	// repeat loop
+	do{
+		// input
+		cout << "Enter your sentence, word or phrase: \n";
+		getline (cin, phrase);
+
+		// processing
+		istringstream iss(phrase);
+		string word;
+
+		while (iss >> word)
+		{
+			for (i = word.length(); i >= 0; i--)
+			{
+					cout << word[i] << endl;
+			}
+		}
+
+		cout << endl;
+
+		// ask to repeat
+		cout << "Would you like to reverse something else? \n";
+		cin  >> repeat;
+
+	} while (repeat == 'Y' || repeat == 'y');
+
+	cout << "Goodbye!" << endl;
+
+	return 0;
+} 
